96dc1f783c22d010c7fae7407ddbdbd8bb61a68d
Pradyumna <pradyumn.bisoyi.java@gail.com>
2022-05-27 04:16:44 +0530

    Test changes | release

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hii");
+        System.out.println("Release");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,6 @@
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("Hii");
+        System.out.println("Release");
+        System.out.println("Hello");
     }
 }
