46f4ef925d2bf4eedfb19f42e8166f3a34084464
Pradyumna <pradyumn.bisoyi.java@gail.com>
2022-05-27 04:24:00 +0530

    Test4 | develop

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hii develop");
+        int i = 0;
+        while (i < 3) {
+            System.out.println(i--);
+        }
     }
 }
