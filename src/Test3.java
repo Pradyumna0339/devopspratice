b09a58f501f6a7e8c63ae3efaed4e7a489cb0ff4
Pradyumna <pradyumn.bisoyi.java@gail.com>
2022-05-27 05:42:21 +0530

    Test3.java | release3

--- src/Test3.java
+++ src/Test3.java
@@ -1,6 +1,6 @@
 public class Test3 {
     public static void main(String[] args) {
-        System.out.println("Release");
+        System.out.println("Release3");
         for (int i = 3; i < 10; i++) {
             System.out.println(i);
         }
